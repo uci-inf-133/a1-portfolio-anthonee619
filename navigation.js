3e9331139ac43c57a0894477f907aca3e7fb47c8
anthonee619 <anthonee619@gmail.com>
2018-10-11 22:50:29 -0700

    Lots of stuff

---
+++ navigation.js
@@ -0,0 +1,15 @@
+  var menu = document.querySelector('nav'),
+    menulink = document.getElementById('hamburger'),
+    aboutMe = document.getElementById("aboutMeNav"),
+    projects = document.getElementById("projectsNav"),
+    home = document.getElementById("homeNav");
+
+  function toggleMenu() {
+    menu.classList.toggle('active');
+    // e.preventDefault();
+  };
+
+  aboutMe.addEventListener('click', toggleMenu);
+  menulink.addEventListener('click', toggleMenu);
+  projects.addEventListener('click', toggleMenu);
+  home.addEventListener('click', toggleMenu);
