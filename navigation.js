511c07bf6d4b0e641475bde7a48e3a763fa65868
anthonee619 <anthonee619@gmail.com>
2018-10-12 20:23:20 -0700

    Added responsive design

--- navigation.js
+++ navigation.js
@@ -3,12 +3,13 @@
     aboutMe = document.getElementById("aboutMeNav"),
     projects = document.getElementById("projectsNav"),
     home = document.getElementById("homeNav");
+
   function toggleMenu() {
     menu.classList.toggle('active');
     // e.preventDefault();
   };
 
-  aboutMe.addEventListener('click', toggleMenu);
   menulink.addEventListener('click', toggleMenu);
+  aboutMe.addEventListener('click', toggleMenu);
   projects.addEventListener('click', toggleMenu);
   home.addEventListener('click', toggleMenu);
