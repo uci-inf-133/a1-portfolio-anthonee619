f3f1f331d355eb7673eabe97a2ed65d17e51bf7a
anthonee619 <anthonee619@gmail.com>
2018-10-12 20:24:58 -0700

    Revert "Added responsive design"
    This reverts commit 511c07bf6d4b0e641475bde7a48e3a763fa65868.

--- navigation.js
+++ navigation.js
@@ -3,13 +3,12 @@
     aboutMe = document.getElementById("aboutMeNav"),
     projects = document.getElementById("projectsNav"),
     home = document.getElementById("homeNav");
-
   function toggleMenu() {
     menu.classList.toggle('active');
     // e.preventDefault();
   };
 
-  menulink.addEventListener('click', toggleMenu);
   aboutMe.addEventListener('click', toggleMenu);
+  menulink.addEventListener('click', toggleMenu);
   projects.addEventListener('click', toggleMenu);
   home.addEventListener('click', toggleMenu);
